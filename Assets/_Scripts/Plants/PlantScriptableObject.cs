bb5e00d633c5dc7e63f3f8090bfe663a7fea2948
ehern120 <ehern120@its.jnj.com>
2022-06-13 18:13:19 +0200

    Create DayManager.cs

--- Assets/_Scripts/Plants/PlantScriptableObject.cs
+++ Assets/_Scripts/Plants/PlantScriptableObject.cs
@@ -8,8 +8,7 @@ namespace _Scripts.Plants
     {
         [field: SerializeField] public string PlantName { get; set; }
         [field: SerializeField] public int AmountOfDaysToGrow { get; set; }
-        [field: SerializeField] public int AmountOfPlantStages { get; set; }
-
+        [field: SerializeField] public List<int> AmountOfGrowthStages { get; set; }
         [field: SerializeField]
         [field: Range(0f, 1)]
         public float BaseSicknessChance { get; set; }
@@ -17,8 +16,13 @@ namespace _Scripts.Plants
         [field: SerializeField] public List<string> PositivePlantEffects { get; set; }
         [field: SerializeField] public List<string> NegativePlantEffects { get; set; }
         
-        [field: SerializeField] public Mesh PlantMesh { get; set; }
+        [field: SerializeField] public List<Mesh> PlantMeshes { get; set; }
         [field: SerializeField] public Material PlantMaterial { get; set; }
         
     }
+}
+
+public class GrowthStages
+{
+    public int AmountOfDaysToReachPlantStage, GrowthStage;
 }
