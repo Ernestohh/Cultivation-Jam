c2df10b035a52a932735de67a886f8ff6a74b603
Hüseyin Baldöktü <Bosiyin@gmail.com>
2022-06-21 00:56:12 +0300

    tpscontroller-changed-little

--- Assets/_Scripts/Player/TPSMovement.cs
+++ Assets/_Scripts/Player/TPSMovement.cs
@@ -7,8 +7,14 @@ public class TPSMovement : MonoBehaviour
     public static TPSMovement Instance = null;
     CharacterController controller;
     [SerializeField] Transform cam;
-    [SerializeField] float speed = 6f;
+    [SerializeField] float walkSpeed = 2f;
+    [SerializeField] float runSpeed = 5.33f;
     [SerializeField] float turnSmoothTime = 0.1f;
+    public float SpeedChangeRate = 10.0f;
+    private float _speed;
+    private float _animationBlend;
+    private float targetAngle = 0.00f;
+
     float turnSmoothVelocity;
     public bool canControlPlayer = true;
     private void Awake()
@@ -32,20 +38,46 @@ public class TPSMovement : MonoBehaviour
     {
         if (canControlPlayer)
         {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-            if (direction.magnitude >= 0.1f)
-            {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                controller.Move(moveDir.normalized * speed * Time.deltaTime);
-            }
+            Move();
         }
+     
+    }
+    void Move()
+    {
+        float targetSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontal, vertical);
+        if (inputVector == Vector2.zero) targetSpeed = 0.0f;
+
+        float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
+            currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            _speed = Mathf.Lerp(_speed, targetSpeed, Time.deltaTime * SpeedChangeRate);
+            Debug.Log("current horizontal speed " + _speed + " target speed " + targetSpeed + " artýþ hýzý " + Time.deltaTime * SpeedChangeRate);
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
+        }
+        else
+        {
+            _speed = targetSpeed;
+        }
+        _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+        if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+   
+        if (inputVector != Vector2.zero)
+        {
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        controller.Move(moveDir.normalized * _speed /*walkSpeed*/ * Time.deltaTime);
     }
 }
